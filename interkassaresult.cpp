bb27f0bc9217e2c4fc21e1c8d1bf5fe7da8500dc
Alexander Zvoznikov <a.zvoznikov@ispsystem.com>
2015-09-14 16:18:28 +0800

    Зачисление платежей

--- interkassaresult.cpp
+++ interkassaresult.cpp
@@ -32,6 +32,33 @@ public:
 	}
 	
 	virtual void Process() {
+		if (Input("ik_co_id") != Method("ik_co_id")) {
+			throw mgr_err::Error("unknow_provider");
+		}
+		
+		if (Input("ik_am") != Payment("amount")) {
+			throw mgr_err::Value("amonut");
+		}
+		
+		if (str::Lower(Input("ik_inv_st")) == "process") {
+			string ik_sign = Input("ik_sign");
+			string sing_str;
+			
+			ForEachI(input_params, ip) {
+				if (str::StartsWith(ip->first, "ik_")) {
+					str::inpl::Append(sing_str, ip->second, ":");	
+				}
+			}
+			
+			sing_str += ":" + Method("secret");
+			string sing = str::base64::Encode(mgr_hash::md5(sing_str));
+			
+			if (sing != ik_sign) {
+				throw mgr_err::Value("signature");
+			} else {
+				SetPaid(MapToStr(input_params, "\n", ":\t"));
+			}
+		}
 	}
 };
 
